94b615463f02f6a60eb34d227469f348a85791f1
Henrique <you@example.com>
2022-09-15 21:30:41 -0300

    novos arquivos docker file e docker compose alteracao na main para verificacao na inicializacao do sistema

--- raspbery/main.py
+++ raspbery/main.py
@@ -1,48 +1,102 @@
+
 from pymongo import MongoClient
 from datetime import datetime
 import cv2
 import os
+
+id= 1
 client = MongoClient("mongodb://henriquegf12:jgs3er4b@192.168.15.38:27017")
 
 
-if __name__ == "__main__":
-    db = client.SmartCam
-    idConfig = 0
+def escreveLogDB(tipo,desc,db):
+    doc =  {"tipo": tipo,
+            "desc": desc,
+            "date": datetime.now()
+            }
+    log = db.log_app_raspberry
+    log.insert_one(doc)
+def criaClassificadoresOpenCV(pathHaarcascadeFrontalFace,pathClassificador):
+    try:
+        classificadorFaces = cv2.CascadeClassifier(pathHaarcascadeFrontalFace)
+        identificadorFaces = cv2.face.EigenFaceRecognizer_create()
+        identificadorFaces.read(pathClassificador)
+        escreveLogDB("INI","Classificadores do OpenCV setados OK [5/QTD_VERI]",db)
+        return True, classificadorFaces,identificadorFaces
+    except:
+        escreveLogDB("ERROR","Erro na criação dos classificadores OpenCV",db)
+        return False, "", ""
 
-    systemON = db.config.find({"id": idConfig})[0]['sistemaLigado']
-    pathClassificador = db.config.find({"id": idConfig})[0]['pathClassificador']
-    pathHaarcascadeFrontalFace = db.config.find({"id": idConfig})[0]['pathHaarcascadeFrontalFace']
-    enderecoCamera = db.config.find({"id": idConfig})[0]['enderecoCamera']
 
-    if(systemON):
-        arquivoClassificador = os.path.exists(pathClassificador)
-        arquivoHaarcascadeFrontalFace = os.path.exists(pathHaarcascadeFrontalFace)
+def verificaArquivosOPENCV(pathClassificador,pathHaarcascadeFrontalFace):
+    if(os.path.exists(pathClassificador) and os.path.exists(pathHaarcascadeFrontalFace)):
+        escreveLogDB("INI","Arquivos OpenCV OK [3/QTD_VERI]",db)
+        return True
+    else:
+        escreveLogDB("ERROR","Não encontrado arquivos OpenCV",db)
+        return False
+def verificaPastaArquivosFotos(pathArquivosFotos):
+    if(os.path.exists(pathArquivosFotos)):
+        escreveLogDB("INI","Pasta Arquivos Fotos OK [4/QTD_VERI]",db)
+        return True
+    else:
+        escreveLogDB("ERROR","Não encontrado pasta arquivos Fotos",db)
+        return False
 
-        if(arquivoClassificador and arquivoHaarcascadeFrontalFace):
-            classificadorFaces = cv2.CascadeClassifier(pathHaarcascadeFrontalFace)
-            identificadorFaces = cv2.face.EigenFaceRecognizer_create()
-            identificadorFaces.read(pathClassificador)
-        else:
-            doc =  {"tipo": "ERRO",
-                    "desc": "Arquivos de configuração não encontrados(Identificador Faces = " + str(arquivoHaarcascadeFrontalFace) + " Reconhecimento Faces = " + str(arquivoClassificador),
-                    "date": datetime.now()
-                    }
-            log = db.log_app_rapsberry
-            log.insert_one(doc)
-            systemON = False
-        
-        camera = cv2.VideoCapture(enderecoCamera)
+def verificaConexaoBD():
+    try:
+        db = client.SmartCam
+        escreveLogDB("INI","Inicialização do Sistema [0/QTD_VERI]",db)
+        escreveLogDB("INI","Conexão BD OK [1/QTD_VERI]",db)
+        return True, db
+    except:
+        print("An exception occurred") 
+        return False, ""
+
+def instanciaCamera(endereCamera):
+    try:
+        camera = cv2.VideoCapture(endereCamera)
         success, frame = camera.read()
+        if(success):
+            escreveLogDB("INI","Camera Funcionando [6/QTD_VERI]",db)
+            return True, camera
+        else:
+            escreveLogDB("ERROR","Camera não Funcionando",db)
+            return False, ""
+    except:
+        escreveLogDB("ERROR","Camera não instanciada",db)
+        return False, ""
+    
 
-        if not success:
-            doc =  {"tipo": "ERRO",
-                    "desc": "Camera não Encontrada",
-                    "date": datetime.now()
-                    }
-            log = db.log_app_rapsberry
-            log.insert_one(doc)
-            systemON = False
 
+def buscaConfigs(idConfig):
+    try:
+        config = db.config.find({"id": idConfig})[0]
+        if( config['sistemaLigado'] != '' 
+        and config['pathClassificador']!= '' 
+        and config['pathHaarcascadeFrontalFace']!= '' 
+        and config['enderecoCamera']!= ''
+        and config['pathArquivosFotos'] != '' ):
+            escreveLogDB("INI","Documento Config OK [2/QTD_VERI]",db)
+            return True, config
+        else:
+            escreveLogDB("ERROR","Documento Config com erro",db)
+            return False, config
+    except:
+        escreveLogDB("ERROR","Documento Config não encontrado ",db)
+        return False, config
 
-    while(systemON):
-        print("eu ")
+         
+if __name__ == "__main__":
+    statusConexaoBD, db = verificaConexaoBD()
+    if(statusConexaoBD):
+        statusConfig, config = buscaConfigs(id)
+        if(statusConfig):
+            systemON = config['sistemaLigado'] 
+            pathClassificador = config['pathClassificador']
+            pathHaarcascadeFrontalFace = config['pathHaarcascadeFrontalFace']
+            enderecoCamera = config['enderecoCamera']
+            pathArquivosFotos = config['pathArquivosFotos']
+            if(verificaArquivosOPENCV(pathClassificador,pathHaarcascadeFrontalFace) and verificaPastaArquivosFotos(pathArquivosFotos)):
+                statusClassificadores,classificadorFaces,identificadorFaces = criaClassificadoresOpenCV(pathClassificador,pathHaarcascadeFrontalFace)
+                if(statusClassificadores):
+                    statusCamera, camera= instanciaCamera(enderecoCamera)
