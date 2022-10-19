eef1256fcae6bfd7b728e974d3cd1022e09ecee0
Henrique <henriquegf12@gmail.com>
2022-10-19 19:40:02 -0300

    altera;oes note

--- raspbery/BancoDados.py
+++ raspbery/BancoDados.py
@@ -5,12 +5,18 @@ class BancoDados:
     def __init__(self,usuario,senha,ip,porta):
         ConectionString = "mongodb://" + usuario + ":" + senha + "@" + ip + ":" + porta
         self.client = MongoClient(ConectionString)
+        self.db = self.client.SmartCam
 
-    def escreveLogDB(tipo,desc,db):
+    #Funções de Escrita 
+    def escreveLogDB(self,tipo,desc):
         doc =  {"tipo": tipo,
                 "desc": desc,
                 "date": datetime.now()
                 }
-        log = db.log_app_raspberry
+        log = self.db.log_app_raspberry
         log.insert_one(doc)
 
+    #Funções de Consulta
+    def buscaConfigbyId(self, idConfig):
+        config = self.db.config.find({"id": idConfig})[0]
+        return config
